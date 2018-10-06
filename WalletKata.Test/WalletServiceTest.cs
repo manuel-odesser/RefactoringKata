939cdcd77a3d063f4896e40e65555599c0295d72
Manuel Odesser <manuel.odesser@engie.com>
2018-10-06 23:29:25 +0200

    UT: simple case when the user is not logged in.

--- WalletKata.Test/WalletServiceTest.cs
+++ WalletKata.Test/WalletServiceTest.cs
@@ -1,13 +1,40 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using WalletKata.Exceptions;
+using WalletKata.Users;
+using WalletKata.Wallets;
 
 namespace WalletKata.Test
 {
     public class WalletServiceTest
     {
-        [Test]
-        public void TestSomething()
-        {
+        private WalletService sut;
+        private Mock<UserSessionRepository> userSessionRepositoryMock;
+        private Mock<UserSession> userSessionMock;
 
+        [SetUp]
+        public void Before()
+        {
+            this.userSessionRepositoryMock = new Mock<UserSessionRepository>();
+            this.userSessionMock = new Mock<UserSession>();
+            this.sut = new WalletService(userSessionRepositoryMock.Object, new WalletDAO());
+        }
+
+        [Test]
+        public void GetWalletsByUser_userIsNotLogged_throws()
+        {
+            // Arrange
+            userSessionMock.Setup(m => m.GetLoggedUser()).Returns((User)null);
+
+            var userSession = this.userSessionMock.Object;
+            userSessionRepositoryMock.Setup(m => m.Current).Returns(userSession);
+
+
+            // Act
+            TestDelegate action = () => { this.sut.GetWalletsByUser(new User()); };
+
+            // Assert
+            Assert.That(action, Throws.TypeOf<UserNotLoggedInException>());
         }
     }
 }
